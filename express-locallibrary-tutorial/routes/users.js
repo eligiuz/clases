204c48f406af8decad8383e7cfeaf652710f68e8
Eligio Cachón Menéndez <ecachon@gmail.com>
2018-12-04 16:55:50 -0600

    devmozilla

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    'use strict';
+    res.send('respond with a resource');
 });
+
+/* Agregar cool */
+router.get('/cool', function (req, res, next) {
+    'use strict';
+    res.send('You\'re so cool');
+});
+
 
 module.exports = router;
